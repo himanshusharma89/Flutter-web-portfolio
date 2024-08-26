74ad646c18bf71dfc977adce65cb1e507bfac3b4
Himanshu Sharma <contact@himanshusharma.tech>
2024-08-26 11:56:36 +0530

    url launcher method update

--- lib/helpers/launcher.dart
+++ lib/helpers/launcher.dart
@@ -1,9 +1,9 @@
-import 'package:url_launcher/url_launcher_string.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Launcher {
   Future<dynamic> launchURL(String url) async {
-    if (await canLaunchUrlString(url)) {
-      await launchURL(url);
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
     } else {
       throw Exception('Could not launch $url');
     }
